75347975c9cc49a00a56b47365d2d5814ee7960b
Aminu5685 <aminu5685@gmail.com>
2022-12-21 06:39:46 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/* a C program to print a string */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:  Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
