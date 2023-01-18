9b504501df03fb77c61e59a3b6344eb18ca31401
Aminu5685 <aminu5685@gmail.com>
2023-01-18 02:12:57 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string.
+ * @str: input string.
+ */
+
+void puts_half(char *str)
+{
+	int j = 0, i;
+
+	while (j >= 0)
+	{
+		if (str[j] == '\0')
+			break;
+		j++;
+	}
+
+	if (j % 2 == 1)
+		i = j / 2;
+	else
+		i = (j - 1) / 2;
+
+	for (i++; i < j; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
